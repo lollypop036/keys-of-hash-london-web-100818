e427bdc466dd8a69ab790f1e82a80545a381c310
Lola Onanuga <slonanuga@gmail.com>
2018-10-04 20:44:50 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require "pry" 
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  
+  def keys_of(*arguments)
+    array = []
+    keys = self.keys
+    
+    arguments.each do |country|
+        keys.each do |item|
+  
+          if self[item] == country
+             array.push(item)
+          end
+        end
+      end
+  
+array 
+end
+
 end
