ba8ba150f16a66939ccf2518b95f2d5c3b01a928
Lola Onanuga <slonanuga@gmail.com>
2018-10-04 20:42:07 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,12 +5,12 @@ class Hash
   def keys_of(*arguments)
     array = []
     keys = self.keys
-    arguments.each do
+    arguments.each do |country|
     keys.each do |item|
   # binding.pry
-  if self[item] == arguments
+  if self[item] == country
     array.push(item)
-
+end
 end
 end
   
